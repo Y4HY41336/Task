d596da6ae2bf76b484b9430548f9f51b0ee196ee
Yahya <148375737+Y4HY41336@users.noreply.github.com>
2023-11-03 00:18:16 +0400

    Taskleri tezeden yukledim
      Ilk basda taski sehv atdigim ucun tezeden yukledim

---
+++ Value&Referance types. Array/Task/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿void Resizer(ref int[] array, int newSize)
+{
+    int[] newArray = new int[newSize];
+    int lenght = 0;
+    if (newSize < array.Length) 
+    {
+        lenght = newSize;
+    }
+    else
+    {
+        lenght = array.Length;
+    }
+    for (int i = 0; i < lenght; i++)
+    {
+        newArray[i] = array[i];
+    }
+    array = newArray;
+}
+int[] nums = { 1, 2, 3, 4, 5, 6};
+Console.WriteLine("Before");
+foreach (var item in nums)
+{
+    Console.WriteLine(item);
+}
+Resizer(ref nums, 8);
+Console.WriteLine("After");
+foreach (var item in nums)
+{
+    Console.WriteLine(item);
+}
