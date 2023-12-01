47cce52580acaa49fc9bfc05085ebd09c681573f
Yahya <148375737+Y4HY41336@users.noreply.github.com>
2023-12-01 17:30:20 +0400

    Add files via upload

---
+++ Console project/Task02/CustomList.cs
@@ -0,0 +1,101 @@
+﻿using System.Drawing;
+namespace Algorithms;
+public class CustomList<T>
+{
+    private T[] list;
+    private int count = 0;
+    private int capacity;
+    public int Count { get { return count; } }
+    public int Capacity { get { return capacity; } }
+    public CustomList()
+    {
+        int capacity = 4;
+        this.capacity = capacity;
+        list = new T[capacity];
+    }
+    public CustomList(int capacity)
+    {
+        if (capacity < 1) capacity = 1;
+        this.capacity = capacity;
+        list = new T[capacity];
+    }              
+    public void Add(T newElement)
+    {
+        if (count == capacity)
+        {
+            T[] resized = new T[capacity * 2];
+            for (int i = 0; i < capacity; i++)
+            {
+                resized[i] = list[i];
+            }
+            list = resized;
+            capacity = capacity * 2;
+        }
+
+        list[count] = newElement;
+        count++;
+    }
+    public T? Find(Predicate<T> match)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (match(list[i]))
+            {
+                return list[i];
+            }
+        }
+        return default;
+    }
+    public CustomList<T> FindAll(Predicate<T> match)
+    {
+        CustomList<T> list = new CustomList<T>();
+        for (int i = 0; i < count; i++)
+        {
+            if (match(this.list[i]))
+            {
+                list.Add(this.list[i]);
+            }
+        }
+        return list;
+    }
+    public bool Contains(T item)
+    {                 
+        if (count != 0 && Array.IndexOf(list, item) != -1)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void Delete(int index)
+    {
+        for (int i = index; i < count - 1; i++)
+        {
+            list[i] = list[i + 1];
+        }
+
+        list[count - 1] = default(T);
+        count--;
+    }
+    public bool Remove(T item)
+    {
+        int index = Array.IndexOf(list,item);
+        if (index >= 0)
+        {
+            Delete(index);
+            return true;
+        }
+        return false;
+    }
+    public bool Exists(Predicate<T> match)
+    {
+        foreach(T item in list)
+        {
+            if (match(item))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
---
+++ Console project/Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿using Algorithms;
+CustomList<int> customList = new CustomList<int>();
+customList.Add(1);
+customList.Add(2);
+customList.Add(3);
+customList.Add(4);
+customList.Add(5);
+customList.Add(6);
+customList.Add(7);
+customList.Add(8);
+customList.Add(9);
+//Console.WriteLine(customList.Find(x => x % 2 == 0));
+//Console.WriteLine(customList.FindAll(x => x % 2 == 0).Count);
+//Console.WriteLine(customList.Contains(5));
+//Console.WriteLine(customList.Remove(4));
+//Console.WriteLine(customList.Exists(x => x % 2 == 0));
